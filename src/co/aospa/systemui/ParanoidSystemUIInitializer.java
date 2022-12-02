9531109ba1b651d4fdaf02e375aa449378ae06b2
Chris Crump <chriscrawford893@gmail.com>
2022-12-02 00:38:29 -0500

    Initial Paranoid SystemUI module
    Based on and referenced from https://github.com/StatiXOS/android_packages_apps_Statix_SystemUI
    
    Change-Id: I99689c4b9e3bdd45ca849e7e199c99fe1b98a387

---
+++ src/co/aospa/systemui/ParanoidSystemUIInitializer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package co.aospa.systemui;
+
+import android.content.Context;
+
+import com.android.systemui.SystemUIInitializer;
+import com.android.systemui.dagger.GlobalRootComponent;
+
+import co.aospa.systemui.dagger.DaggerParanoidGlobalRootComponent;
+
+public class ParanoidSystemUIInitializer extends SystemUIInitializer {
+    public ParanoidSystemUIInitializer(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected GlobalRootComponent.Builder getGlobalRootComponentBuilder() {
+        return DaggerParanoidGlobalRootComponent.builder();
+    }
+}
