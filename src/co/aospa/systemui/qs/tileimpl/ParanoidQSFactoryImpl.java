2b80629f701357761e579eb8519cb1cdfb7181f8
Adithya <gh0strider.2k18.reborn@gmail.com>
2023-02-09 08:33:17 -0300

    ParanoidSystemUI: Implement Bluetooth dialog
    Based on Internet dialog code.
    
    Change-Id: Iff63a49ff6a49c535b63a430bee8d7c55e78cbcc
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/co/aospa/systemui/qs/tileimpl/ParanoidQSFactoryImpl.java
+++ src/co/aospa/systemui/qs/tileimpl/ParanoidQSFactoryImpl.java
@@ -25,7 +25,6 @@ import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AlarmTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
-import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CameraToggleTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
@@ -57,6 +56,7 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 
 import co.aospa.systemui.qs.tiles.AlwaysOnDisplayTile;
+import co.aospa.systemui.qs.tiles.BluetoothDialogTile;
 import co.aospa.systemui.qs.tiles.CaffeineTile;
 import co.aospa.systemui.qs.tiles.DataSwitchTile;
 import co.aospa.systemui.qs.tiles.DcDimmingTile;
@@ -80,7 +80,7 @@ public class ParanoidQSFactoryImpl extends QSFactoryImpl {
             Provider<CustomTile.Builder> customTileBuilderProvider,
             Provider<WifiTile> wifiTileProvider,
             Provider<InternetTile> internetTileProvider,
-            Provider<BluetoothTile> bluetoothTileProvider,
+            Provider<BluetoothDialogTile> bluetoothTileProvider,
             Provider<CellularTile> cellularTileProvider,
             Provider<DndTile> dndTileProvider,
             Provider<ColorInversionTile> colorInversionTileProvider,
@@ -115,7 +115,7 @@ public class ParanoidQSFactoryImpl extends QSFactoryImpl {
             Provider<DcDimmingTile> dcDimTileProvider,
             Provider<PowerShareTile> powerShareTileProvider) {
         super(qsHostLazy, customTileBuilderProvider, wifiTileProvider, internetTileProvider,
-                bluetoothTileProvider, cellularTileProvider, dndTileProvider,
+                bluetoothTileProvider::get, cellularTileProvider, dndTileProvider,
                 colorInversionTileProvider, airplaneModeTileProvider, workModeTileProvider,
                 rotationLockTileProvider, flashlightTileProvider, locationTileProvider,
                 castTileProvider, hotspotTileProvider, batterySaverTileProvider,
