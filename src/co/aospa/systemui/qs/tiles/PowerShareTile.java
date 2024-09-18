b207d9b1d0044679ef88ddde374e90db2a884079
Dhina17 <dhinalogu@gmail.com>
2024-09-18 16:07:29 +0530

    ParanoidSystemUI: QS: Update tiles to Android 15
    Change-Id: I3baab62aa98a16ddf842dbcdbf4a7a963f0ff6f1
    Signed-off-by: Fazil Sheik <fazil.sheik96@gmail.com>

--- src/co/aospa/systemui/qs/tiles/PowerShareTile.java
+++ src/co/aospa/systemui/qs/tiles/PowerShareTile.java
@@ -26,11 +26,11 @@ import android.os.Looper;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.service.quicksettings.Tile;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.animation.Expandable;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.plugins.ActivityStarter;
@@ -155,7 +155,7 @@ public class PowerShareTile extends QSTileImpl<BooleanState>
     }
 
     @Override
-    public void handleClick(@Nullable View view) {
+    public void handleClick(@Nullable Expandable expandable) {
         try {
             boolean powerShareEnabled = mPowerShare.isEnabled();
 
